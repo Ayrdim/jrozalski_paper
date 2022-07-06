4370005fc21c0e8d881dd1ad22060885ebb83f3c
Ryan Dimsey <ryandimsey@gmail.com>
2022-07-06 21:59:13 +1000

    Changed to 'unrecoverable' error handling

--- src/utils.rs
+++ src/utils.rs
@@ -1,55 +1,59 @@
 use reqwest;
 use scraper::{Html, Selector};
-use std::error::Error;
 use std::process::Command;
 
-pub async fn from_here_get_this(
-    url: &str,
-    parse: &str,
-    attr: &str,
-) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut v: Vec<String> = Vec::new();
-
-    let response = reqwest::get(url).await?;
+pub async fn from_here_get_this(url: &str, parse: &str, attr: &str) -> Option<Vec<String>> {
+    let response = reqwest::get(url)
+        .await
+        .expect(&format!("Failed to get HTML from '{}'", url));
 
     // We havent received an error but we cannot continue if the status code isnt OK
-    if reqwest::StatusCode::OK != response.status() {
-        return Err("Request status code was not OK.".into());
-    }
+    if reqwest::StatusCode::OK == response.status() {
+        let mut attribute_values: Vec<String> = Vec::new();
+        let html = response
+            .text()
+            .await
+            .expect("Failed to parse HTML response to text");
 
-    let html = response.text().await?;
-    let document = Html::parse_document(&html);
+        let document = Html::parse_document(&html);
+        let selector = Selector::parse(parse).expect("Failed to create parser");
+        let selections = document.select(&selector);
 
-    let selector = match Selector::parse(parse) {
-        Ok(selector) => selector,
-        Err(_) => return Err("Request status code was not OK.".into()),
-    };
-
-    let selections = document.select(&selector);
-
-    for element in selections {
-        if let Some(target_url) = element.value().attr(attr) {
-            v.push(target_url.to_owned());
+        for element in selections {
+            if let Some(target_url) = element.value().attr(attr) {
+                attribute_values.push(target_url.to_owned());
+            }
         }
+
+        return Some(attribute_values);
     }
 
-    return Ok(v);
+    None
 }
 
-pub async fn download_image(url: &str, save_to: &str) -> Result<(), Box<dyn Error>> {
-    let image_bytes = reqwest::get(url).await?.bytes().await?;
-    image::load_from_memory(&image_bytes)?.save(&save_to)?;
+pub async fn download_image(url: &str, save_to: &str) {
+    let image_bytes = reqwest::get(url)
+        .await
+        .expect(&format!("Failed to get HTML from '{}'", url))
+        .bytes()
+        .await
+        .expect("Failed to convert HTML to bytes");
 
-    Ok(())
+    image::load_from_memory(&image_bytes)
+        .expect("Failed to get load bytes as image")
+        .save(&save_to)
+        .expect(&format!("Failed to save image as '{}'", save_to));
 }
 
-pub fn set_desktop_backgound(image_path: &str) -> Result<(), Box<dyn Error>> {
+pub fn set_desktop_backgound(image_path: &str) {
     Command::new("gsettings")
         .arg("set")
         .arg("org.gnome.desktop.background")
         .arg("picture-uri-dark")
         .arg(format!("file://{}", &image_path))
-        .output()?;
-
-    Ok(())
+        .output()
+        .expect(&format!(
+            "Failed to set '{}' image as background",
+            image_path
+        ));
 }
